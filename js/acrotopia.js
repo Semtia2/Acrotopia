7c3ab1b21f0ebde71136f5d088980f131faad250
Koosha Jahani <kooshajahani@yahoo.com>
2021-12-18 02:31:43 -0800

    can now submit responses in both acronym writer and judger

--- js/acrotopia.js
+++ js/acrotopia.js
@@ -8,6 +8,24 @@ $(window).ready(function() {
 // Will pretend to be server until I finish server side code
 // Afterwards I'll replace it with server code
 var dummyServer = {
+    acronymResponses: [
+        {player: "AOoOGH", acronym: "My love for you is bulletproof but you're the one who shot me"},
+        {player: "Balcony nut", acronym: "You must stick up for yourself son"},
+        {player: "Dude From Highschool", acronym: "I wrote Jesus Walks, I'm never gonna hell"},
+        {player: "BioDad", acronym: "I can't believe that it's always like this"}
+    ],
+
+    pushToServer: function(message) {
+        if (message.type !== 'utf8')
+            return
+
+        var clientMessage = JSON.parse(message.utf8Data);
+        switch (clientMessage.type) {
+            case "acronym":
+                dummyServer.acronymResponses.push({player:"Porto", acronym:clientMessage.acronym})
+                break;
+        }
+    }
 
 }
 
@@ -24,9 +42,24 @@ var game = {
     lenient: false,
     singleVote: false,
     kick: false,
+    acronymLetters: [],
+    playerList: [
+        {name:"AOoOGH!", score:0},
+        {name:"Balcony nut", score:0},
+        {name:"Dude From Highschool", score:0},
+        {name:"BioDad", score:0},
+        {name:"Porto", score:0},
+        {name:"Splante", score:0},
+        {name:"Zyberzky Ewe", score:0},
+    ],
+    acronymResponses: [
+        {player: "", acronym: "My love for you is bulletproof but you're the one who shot me"},
+        {player: "Balcony nut", acronym: "You must stick up for yourself son"},
+        {player: "", acronym: "I wrote Jesus Walks, I'm never gonna hell"},
+        {player: "", acronym: "I can't believe that it's always like this"}
+    ],
 
     init: function() {
-
         var coll = document.getElementsByClassName("collapsible");
         var i;
 
@@ -46,40 +79,21 @@ var game = {
     },
 
     startGame: function() {
+        game.initializeAcronymWriter();
+        game.initializeAcronymJudger();
         $('#welcomecontainer').hide();
+        $('#gamecontainer').show();
+    },
 
-        var acronymLetters = [];
+    // Modes: createLobby, waitingRoom, writeAcronyms, judgeAcromyms, results
+    mode: "writeAcronyms",
 
-        for (var _ = 0; _ < game.acronymLength; _++) {
-            acronymLetters.push(game.getRandomLetter())
-        }
-
+    initializeAcronymWriter: function() {
         var $playerCompletionStatusTemplate = $("<div>", {
             class: "playerinfo completionstatus"
         })
-        acronymLetters.map(function(letter) {
-            //this=$("#acronymdisplay")
-            this.append($("<div>", {
-                class: "initials uncompleted",
-                text: letter
-            }))
-            $playerCompletionStatusTemplate.append($("<div>", {
-                class: "statusinitials uncompleted",
-                text: letter
-            }))
-        }, $("#acronymdisplay"))
 
-        var playerList = [
-            {name:"AOoOGH!", score:0},
-            {name:"Balcony nut", score:0},
-            {name:"Dude From Highschool", score:0},
-            {name:"BioDad", score:0},
-            {name:"Porto", score:0},
-            {name:"Splante", score:0},
-            {name:"Zyberzky Ewe", score:0},
-        ];
-
-        playerList.map(function(playerInfo, index, array) {
+        game.playerList.map(function(playerInfo, index, array) {
             var $playerDiv = $("<div>", {
                 class: "player",
                 id: "player"+String(index)
@@ -111,18 +125,44 @@ var game = {
                 .append($playerCompletionStatus)
                 .append($playerKick)
 
-            // this=$("#playerList")
+            // this is defined as $("#playerList")
             this.append($playerDiv)
         }, $("#playerlist"));
 
-        $('#gamecontainer').show();
         game.switchToWrite();
     },
 
-    // Modes: createLobby, waitingRoom, writeAcronyms, judgeAcromyms, results
-    mode: "writeAcronyms",
+    initializeAcronymJudger: function() {
+        game.switchToJudge();
+    },
 
     switchToWrite: function() {
+        game.acronymLetters = [];
+        for (var _ = 0; _ < game.acronymLength; _++) {
+            game.acronymLetters.push(game.getRandomLetter())
+        }
+
+        var $letterDisplayTemplate = $("<div>", {
+            id: "letterdisplay"
+        })
+        var $playerCompletionStatusTemplate = $("<div>", {
+            class: "playerinfo completionstatus"
+        })
+        game.acronymLetters.map(function(letter) {
+            // this is defined as $("#letterdisplay")
+            $letterDisplayTemplate.append($("<div>", {
+                class: "initials uncompleted",
+                text: letter
+            }))
+            $playerCompletionStatusTemplate.append($("<div>", {
+                class: "statusinitials uncompleted",
+                text: letter
+            }))
+        })
+
+        $("#letterdisplay").replaceWith($letterDisplayTemplate);
+        $(".completionstatus").replaceWith($playerCompletionStatusTemplate);
+
         $('#acronymjudger').hide();
         $('#acronymwriter').show();
         var $acronymInput = $('#acronyminput textarea')
@@ -137,13 +177,30 @@ var game = {
                 animate: false,
                 extraSpace : extraLineSpace
             });
+
         game.mode = "writeAcronyms";
     },
 
     switchToJudge: function() {
+        var $acronymDisplayTemplate = $("<div>", {
+            id: "acronymdisplay"
+        })
+        game.acronymResponses.map(function(response) {
+            $acronymDisplayTemplate.append($("<div>", {
+                class: "acronym",
+                text: response.acronym
+            }).on("click", function(){
+                this.classList.toggle("selected")
+                $("#finalizevote").prop('disabled', $("#acronymdisplay .acronym.selected").length < 1);
+            })
+            )
+        })
+        $("#acronymdisplay").replaceWith($acronymDisplayTemplate)
+        $("#acronymdisplay .acronym")
+
         $('#acronymwriter').hide();
         $('#acronymjudger').show();
-        mode = "judgeAcronyms";
+        game.mode = "judgeAcronyms";
     },
 
     getRandomLetter() {
@@ -151,5 +208,25 @@ var game = {
         // Math.random() returns a float from the interval [0,1)
         var charCode = Math.floor(Math.random() * 26) + 65
         return String.fromCharCode(charCode)
+    },
+
+    activateSubmit(response) {
+        if (!response)
+            return
+        var submitButton = $("#acronymsubmit")
+        submitButton.prop('disabled', !game.validAcronym(response));
+    },
+
+    validAcronym(response) {
+        var words = response.split(/ +(?=\S)/)
+        if (words.length == game.acronymLength)
+            return game.acronymLetters.every((curr, index) => curr == words[index][0].toUpperCase())
+        else
+            return false
+    },
+
+    submit() {
+        var messageObject = {type:"acronym", acronym:$('#acronyminput textarea').value}
+        dummyServer.pushToServer(JSON.stringify(messageObject))
     }
 }
